63721425977f37cb766b738d8edfc534382916c7
D0hwQ1 <pjt137@naver.com>
2021-07-21 00:04:30 +0000

    Arranged migrations's '1_logic.js'

--- migrations/1_logic.js
+++ migrations/1_logic.js
@@ -1,5 +1,5 @@
-const Ast = artifacts.require("Logic");
+const Logic = artifacts.require("Logic");
 
 module.exports = function (deployer) {
-  deployer.deploy(Ast, );
+  deployer.deploy(Logic);
 };
