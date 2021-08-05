1f1423b4cf61ad3d03781bf10963dd20ab817e57
CraftyDragon678 <craftydragon678@gmail.com>
2021-08-05 10:23:49 +0900

    init: morgan

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,5 +1,12 @@
+import morgan from "@src/api/middleware/morgan";
 import express from "express";
 
-const expressLoader = ({ app }: { app: express.Application }) => {};
+const expressLoader = ({ app }: { app: express.Application }) => {
+  app
+    .route("/status")
+    .get((req, res) => res.sendStatus(204))
+    .head((req, res) => res.sendStatus(204));
+  app.use(morgan);
+};
 
 export default expressLoader;
