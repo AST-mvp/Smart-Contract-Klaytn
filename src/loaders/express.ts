3b6eb0277c295b34e37a7653cf568fe9326534a6
CraftyDragon678 <craftydragon678@gmail.com>
2021-08-05 10:27:19 +0900

    init: http exception

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,5 +1,6 @@
 import morgan from "@src/api/middleware/morgan";
-import express from "express";
+import HttpException from "@src/exceptions/HttpException";
+import express, { Request, Response, NextFunction } from "express";
 
 const expressLoader = ({ app }: { app: express.Application }) => {
   app
@@ -7,6 +8,14 @@ const expressLoader = ({ app }: { app: express.Application }) => {
     .get((req, res) => res.sendStatus(204))
     .head((req, res) => res.sendStatus(204));
   app.use(morgan);
+  app.use(express.json());
+  app.use(
+    (err: HttpException, req: Request, res: Response, next: NextFunction) => {
+      res.status(err.status ?? 500).json({
+        message: err.message,
+      });
+    }
+  );
 };
 
 export default expressLoader;
