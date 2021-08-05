7d4eb5348f1d54133afeb51715f50c46b17c37f9
CraftyDragon678 <craftydragon678@gmail.com>
2021-08-05 18:11:12 +0900

    fix: change check permission condition
    every -> one of

--- src/api/middleware/checkPermission.ts
+++ src/api/middleware/checkPermission.ts
@@ -2,14 +2,15 @@ import HttpException from "@src/exceptions/HttpException";
 import { UserAttributes } from "@src/model/User";
 import { NextFunction, Request, Response } from "express";
 
+/**
+ * check user has one of permission
+ */
 const checkPermission =
   (...permissions: UserAttributes["permission"]) =>
   (req: Request, res: Response, next: NextFunction) => {
     const { user } = req;
     if (!user) return next(new HttpException(401));
-    if (
-      !permissions.every((permission) => user.permission.includes(permission))
-    )
+    if (!permissions.some((permission) => user.permission.includes(permission)))
       return next(new HttpException(404));
     next();
   };
