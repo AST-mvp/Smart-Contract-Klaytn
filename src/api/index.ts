fa1126fdccc541f47b97d16215ab12a4270d5c97
CraftyDragon678 <craftydragon678@gmail.com>
2021-08-06 20:28:03 +0900

    feat: add nfc router

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import auth from "./routes/auth";
+import nfcs from "./routes/nfcs";
 import products from "./routes/products";
 import users from "./routes/users";
 
 const api = () => {
   const router = Router();
   auth(router);
+  nfcs(router);
   products(router);
   users(router);
   return router;
