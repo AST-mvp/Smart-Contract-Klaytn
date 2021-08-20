57c1183aacb324d74f61c8524522ae4feb458d86
CraftyDragon678 <craftydragon678@gmail.com>
2021-08-20 15:11:15 +0900

    chore: remove unnecessary files

--- src/astCaver.ts
+++ src/astCaver.ts
@@ -1,18 +1,18 @@
-import { caverLogger } from "./loaders/logger";
+import { caverLogger } from './loaders/logger';
 
-const Caver = require("caver-js");
-const ethers = require("ethers");
+const Caver = require('caver-js');
+const ethers = require('ethers');
 const toBytes32 = ethers.utils.formatBytes32String;
-const caver = new Caver("https://api.baobab.klaytn.net:8651/");
+const caver = new Caver('https://api.baobab.klaytn.net:8651/');
 
 const account = caver.wallet.keyring.create(
-  "0xdd6e0b6d3c71c0a43a6ea2cedf91e1e48e194ea6",
-  "0x9bd8e94d3edc022a6d2b5d3e2fb6b55e1d300c72748dfb99db451cd885315373"
+  '0xdd6e0b6d3c71c0a43a6ea2cedf91e1e48e194ea6',
+  '0x9bd8e94d3edc022a6d2b5d3e2fb6b55e1d300c72748dfb99db451cd885315373',
 );
 caver.wallet.add(account);
 
-const abi = require("../build/contracts/Logic.json").abi;
-const cont_addr = require("../build/contracts/Logic.json").networks[1001]
+const abi = require('../build/contracts/Logic.json').abi;
+const cont_addr = require('../build/contracts/Logic.json').networks[1001]
   .address;
 const Ast = new caver.contract(abi, cont_addr);
 
@@ -24,7 +24,7 @@ export async function registerProductInfo(
   manufactureDate: string,
   limited: boolean,
   appeared: boolean,
-  ownerID: string
+  ownerID: string,
 ) {
   return allProductInfo().then((result) => {
     for (var i = 0; i < result.length; i++) {
@@ -41,7 +41,7 @@ export async function registerProductInfo(
         toBytes32(manufactureDate),
         limited,
         appeared,
-        ownerID
+        ownerID,
       )
       .send({
         from: account.address,
@@ -50,12 +50,12 @@ export async function registerProductInfo(
       .then(function (message: any) {
         caverLogger.debug(
           `register product info ${nfcID} ${toBytes32(brandID)} ${toBytes32(
-            productID
+            productID,
           )} ${toBytes32(editionID)} ${toBytes32(
-            manufactureDate
-          )} ${limited} ${appeared} ${ownerID}`
+            manufactureDate,
+          )} ${limited} ${appeared} ${ownerID}`,
         );
-        caverLogger.debug("register product info", message);
+        caverLogger.debug('register product info', message);
       });
     return true;
   });
@@ -63,7 +63,7 @@ export async function registerProductInfo(
 
 export async function allProductInfo() {
   var result = await Ast.methods.allProductInfo().call();
-  caverLogger.debug("all product info", result);
+  caverLogger.debug('all product info', result);
   return result;
 }
 
@@ -78,7 +78,7 @@ export async function changeOwnership(nfcID: number, new_ownerId: string) {
             gas: 250000,
           })
           .then(function (message: any) {
-            caverLogger.debug("change ownership", message);
+            caverLogger.debug('change ownership', message);
           });
         return true;
       }
@@ -101,6 +101,3 @@ export async function nfcIDcheck(nfcID: number) {
 export function to_String(hex: string) {
   return ethers.utils.parseBytes32String(hex);
 }
-
-// registerProductInfo(123456,'1','2','3',"21-07-12", false, false, 4321)
-allProductInfo();
